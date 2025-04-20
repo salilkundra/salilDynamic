58e063fd745131431e39b2a2db084f3da03e6e92
salilkundra <136920548+salilkundra@users.noreply.github.com>
2025-04-20 23:24:01 +0530

    first commit

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,13 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "dtjrdz9oq",
-  api_key: "689511183569294",
-  api_secret: "ntC4n7aienS7OTot2kLzN1fTA2M",
+  // cloud_name: "dtjrdz9oq",
+  // api_key: "689511183569294",
+  // api_secret: "ntC4n7aienS7OTot2kLzN1fTA2M",
+  cloud_name: "dykoodzii",
+  api_key: "456491375924611",
+  api_secret: "7_jhYdh3FAUqxFBhe_6-g4nLX0c",
+  
 });
 
 const storage = new multer.memoryStorage();
